88391b6c7d117c5d42d3a36cd1dff7ea47817d99
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2023-06-06 14:21:22 +0300

    LED Finished

--- application.c
+++ application.c
@@ -44,14 +44,10 @@ led_t data_pin = {
 };
 
 int main() {
-    application_initialize();
-    
-    gpio_port_direction_initialize(PORTA_INDEX, GPIO_DIRECTION_OUTPUT);
-    
+    application_initialize();  
     
     while(1)
     {
-        /*
         led_turn_on(&led_1);
         __delay_ms(50);
         led_turn_off(&led_1);
@@ -61,28 +57,6 @@ int main() {
         led_turn_on(&led_3);
         __delay_ms(50);
         led_turn_off(&led_3);
-        
-        */
-        
-        send_data(0b00000000);
-        __delay_ms(200);
-        send_data(0b10000000);
-        __delay_ms(200);
-        send_data(0b01000000);
-        __delay_ms(200);
-        send_data(0b00100000);
-        __delay_ms(200);
-        send_data(0b00010000);
-        __delay_ms(200);
-        send_data(0b00001000);
-        __delay_ms(200);
-        send_data(0b00000100);
-        __delay_ms(200);
-        send_data(0b00000010);
-        __delay_ms(200);
-        send_data(0b00000001);
-        __delay_ms(200);
-        
     }
 
     return (EXIT_SUCCESS);
@@ -93,35 +67,4 @@ void application_initialize(){
     ret = led_initialize(&led_1);
     ret = led_initialize(&led_2);
     ret = led_initialize(&led_3);
-    
-    ret = led_initialize(&clock_pin);
-    ret = led_initialize(&latch_enable_pin);
-    ret = led_initialize(&data_pin);
-}
-
-void clock_signal(void){
-   led_turn_on(&clock_pin);
-   __delay_us(500);
-   led_turn_off(&clock_pin);
-   __delay_us(500);
-}
-
-void latch_enable(void){
-   led_turn_on(&latch_enable_pin);
-   __delay_us(500);
-   led_turn_off(&latch_enable_pin);
-}
-
-void send_data(uint8 data_out){
-    for (uint8 i=0 ; i<8 ; i++)
-    {
-        if(((data_out >> i) & (0x01)) == 0x01){
-            led_turn_on(&data_pin);
-            clock_signal();
-        }else{
-            led_turn_off(&data_pin);
-            clock_signal();
-        }
-    }
-    latch_enable(); // Data finally submitted
 }
