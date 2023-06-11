3b8552fc172a1f4d93c03a82e966dd2d22b97a57
hp <hp@hp-PC>
2023-06-11 18:45:33 +0200

    DC_Motor_interface

--- application.c
+++ application.c
@@ -8,10 +8,28 @@
 
 #include "application.h"
 
-relay_t relay_1 = {
-    .relay_port_name = PORTC_INDEX,
-    .relay_pin = GPIO_PIN0,
-    .relay_status = RELAY_OFF_STATUS
+dc_motor_t dc_motor_1 = {
+    .dc_motor_pins[DC_MOTOR_PIN_1].port = PORTC_INDEX,
+    .dc_motor_pins[DC_MOTOR_PIN_1].pin = GPIO_PIN0,
+    .dc_motor_pins[DC_MOTOR_PIN_1].direction = GPIO_DIRECTION_OUTPUT,
+    .dc_motor_pins[DC_MOTOR_PIN_1].logic = DC_MOTOR_OFF_STATUS,
+    
+    .dc_motor_pins[DC_MOTOR_PIN_2].port = PORTC_INDEX,
+    .dc_motor_pins[DC_MOTOR_PIN_2].pin = GPIO_PIN1,
+    .dc_motor_pins[DC_MOTOR_PIN_2].direction = GPIO_DIRECTION_OUTPUT,
+    .dc_motor_pins[DC_MOTOR_PIN_2].logic = DC_MOTOR_OFF_STATUS,
+};
+
+dc_motor_t dc_motor_2 = {
+    .dc_motor_pins[DC_MOTOR_PIN_1].port = PORTC_INDEX,
+    .dc_motor_pins[DC_MOTOR_PIN_1].pin = GPIO_PIN2,
+    .dc_motor_pins[DC_MOTOR_PIN_1].direction = GPIO_DIRECTION_OUTPUT,
+    .dc_motor_pins[DC_MOTOR_PIN_1].logic = DC_MOTOR_OFF_STATUS,
+    
+    .dc_motor_pins[DC_MOTOR_PIN_2].port = PORTC_INDEX,
+    .dc_motor_pins[DC_MOTOR_PIN_2].pin = GPIO_PIN3,
+    .dc_motor_pins[DC_MOTOR_PIN_2].direction = GPIO_DIRECTION_OUTPUT,
+    .dc_motor_pins[DC_MOTOR_PIN_2].logic = DC_MOTOR_OFF_STATUS,
 };
 
 int main() {
@@ -20,8 +38,15 @@ int main() {
     application_initialize();
 
     while (1) {
-        relay_toggle(&relay_1);
-        __delay_ms(5000);
+        ret = dc_motor_move_right(&dc_motor_1);
+        ret = dc_motor_move_right(&dc_motor_2);
+        __delay_ms(3000);
+        ret = dc_motor_stop(&dc_motor_1);
+        ret = dc_motor_stop(&dc_motor_2);
+        __delay_ms(3000);
+        ret = dc_motor_move_left(&dc_motor_1);
+        ret = dc_motor_move_left(&dc_motor_2);
+        __delay_ms(3000);
     }
      
     return (EXIT_SUCCESS);
@@ -29,5 +54,6 @@ int main() {
 
 void application_initialize() {
     Std_ReturnType ret = E_NOT_OK;
-    ret = relay_initialize(&relay_1);
+    ret = dc_motor_initialize(&dc_motor_1);
+    ret = dc_motor_initialize(&dc_motor_2);
 }
