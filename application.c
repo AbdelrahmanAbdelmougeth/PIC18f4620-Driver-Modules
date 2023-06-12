cb38929be2f4678572fcfd4fc80b98998cbd5cc7
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2023-06-12 22:08:35 +0200

    BCD_to_7_Segment

--- application.c
+++ application.c
@@ -8,11 +8,6 @@
 
 #include "application.h"
 
-uint8 Seven_Segment_Patterns[10] = {0xC0, 0xF9, 0xA4, 
-                                    0xB0, 0x99, 0x92, 
-                                    0x82, 0xF8, 0x80, 
-                                    0x90};
-
 int main() {
     Std_ReturnType ret = E_NOT_OK;
 
@@ -20,10 +15,8 @@ int main() {
 
     while (1) {
         for(uint8 i=0; i<99; i++){
-            uint8 temp = i;
-            gpio_port_write_logic(PORTD_INDEX, Seven_Segment_Patterns[temp % 10]);
-            temp /= 10;
-            gpio_port_write_logic(PORTC_INDEX, Seven_Segment_Patterns[temp]);
+            uint8 BCD = (((uint8)(i % 10)) | ((uint8)(i / 10)) << 4);
+            gpio_port_write_logic(PORTC_INDEX, BCD);
             __delay_ms(500);
         }  
     }
@@ -34,5 +27,4 @@ int main() {
 void application_initialize() {
     Std_ReturnType ret = E_NOT_OK;
     ret = gpio_port_direction_initialize(PORTC_INDEX, GPIO_DIRECTION_OUTPUT);
-    ret = gpio_port_direction_initialize(PORTD_INDEX, GPIO_DIRECTION_OUTPUT);
 }
