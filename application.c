c7a04cb87231b4ed04e1ff6d04b4123103c6861a
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2023-06-27 18:19:36 +0300

    Shift single line left and right

--- application.c
+++ application.c
@@ -13,19 +13,21 @@ int main() {
     Std_ReturnType ret = E_NOT_OK;
 
     application_initialize();
+    ret = lcd_8bit_send_string_pos(&_lcd_2, 2,1, "Abdelrahman Fathi");
+    ret = lcd_4bit_send_string_pos(&_lcd_1, 2, 1, "Ahmed");
     
-    
+    uint8 lcd1_counter=3;
     while (1) {
-        ret = lcd_8bit_send_string_pos(&_lcd_2,1,1, "Abdelrahman Fathi");
-        ret = lcd_4bit_send_string_pos(&_lcd_1, 1, 1, "Ahmed");
-        ret = lcd_4bit_send_command(&_lcd_1, _LCD_DISPLAY_SHIFT_RIGHT);
-        __delay_ms(250);
-        ret = lcd_4bit_send_command(&_lcd_1, _LCD_DISPLAY_SHIFT_RIGHT);
-        __delay_ms(250);
-        ret = lcd_4bit_send_command(&_lcd_1, _LCD_DISPLAY_SHIFT_LEFT);
-        __delay_ms(250);
-        ret = lcd_4bit_send_command(&_lcd_1, _LCD_DISPLAY_SHIFT_LEFT);
-        __delay_ms(250);
+        for(lcd1_counter; lcd1_counter<=10; lcd1_counter++){
+            ret = lcd_4bit_send_string_pos(&_lcd_1, 1, lcd1_counter, "Hello All"); 
+            __delay_ms(100);
+            ret = lcd_4bit_send_char_data_pos(&_lcd_1, 1, lcd1_counter, ' ');
+        }
+        for(lcd1_counter=10; lcd1_counter>=5; lcd1_counter--){
+            ret = lcd_4bit_send_string_pos(&_lcd_1, 1, lcd1_counter, "Hello All"); 
+            __delay_ms(100);
+            ret = lcd_4bit_send_char_data_pos(&_lcd_1, 1, lcd1_counter+8, ' ');
+        }
     }
      
     return (EXIT_SUCCESS);
