b6ef7f8504fb0552b038884dd3db86852965252f
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2023-06-27 18:55:03 +0300

    Loading message

--- application.c
+++ application.c
@@ -8,26 +8,27 @@
 
 #include "application.h"
 
+void loading_screen(void){
+    lcd_8bit_send_string_pos(&_lcd_2, 1,1, "Loading");
+    __delay_ms(150);
+    while(1){
+    lcd_8bit_send_string_pos(&_lcd_2, 1,8, ".  ");
+    __delay_ms(150);
+    lcd_8bit_send_string_pos(&_lcd_2, 1,8, ".. ");
+    __delay_ms(150);
+    lcd_8bit_send_string_pos(&_lcd_2, 1,8, "...");
+    __delay_ms(150);
+    }
+}
+
 
 int main() {
     Std_ReturnType ret = E_NOT_OK;
 
     application_initialize();
-    ret = lcd_8bit_send_string_pos(&_lcd_2, 2,1, "Abdelrahman Fathi");
-    ret = lcd_4bit_send_string_pos(&_lcd_1, 2, 1, "Ahmed");
     
-    uint8 lcd1_counter=3;
     while (1) {
-        for(lcd1_counter; lcd1_counter<=10; lcd1_counter++){
-            ret = lcd_4bit_send_string_pos(&_lcd_1, 1, lcd1_counter, "Hello All"); 
-            __delay_ms(100);
-            ret = lcd_4bit_send_char_data_pos(&_lcd_1, 1, lcd1_counter, ' ');
-        }
-        for(lcd1_counter=10; lcd1_counter>=5; lcd1_counter--){
-            ret = lcd_4bit_send_string_pos(&_lcd_1, 1, lcd1_counter, "Hello All"); 
-            __delay_ms(100);
-            ret = lcd_4bit_send_char_data_pos(&_lcd_1, 1, lcd1_counter+8, ' ');
-        }
+        loading_screen();
     }
      
     return (EXIT_SUCCESS);
