438929251a08f244a2e05ff0c4f4125d571b9444
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2023-06-27 15:34:28 +0300

    send single character to specific position

--- application.c
+++ application.c
@@ -13,8 +13,11 @@ int main() {
     Std_ReturnType ret = E_NOT_OK;
 
     application_initialize();
-    
     while (1) {
+        ret = lcd_8bit_send_char_data_pos(&_lcd_2, 1,5, 'A');
+        ret = lcd_8bit_send_char_data_pos(&_lcd_2, 2,1, 'B');
+        ret = lcd_8bit_send_char_data_pos(&_lcd_2, 3,5, 'C');
+        ret = lcd_8bit_send_char_data_pos(&_lcd_2, 4,1, 'D');
     }
      
     return (EXIT_SUCCESS);
