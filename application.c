c24ed80ab7377702328c21baa1ced95af1527fd7
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2023-06-27 17:46:25 +0300

    Shift the whole screen left and right

--- application.c
+++ application.c
@@ -13,11 +13,19 @@ int main() {
     Std_ReturnType ret = E_NOT_OK;
 
     application_initialize();
-    ret = lcd_8bit_send_string(&_lcd_2, "Ahmed");
-    ret = lcd_4bit_send_string(&_lcd_1, "Ahmed");
+    
+    
     while (1) {
-        ret = lcd_8bit_send_string_pos(&_lcd_2, 2, 3, "ABDO");
-        ret = lcd_4bit_send_string_pos(&_lcd_1, 2, 3, "ABDO");
+        ret = lcd_8bit_send_string_pos(&_lcd_2,1,1, "Abdelrahman Fathi");
+        ret = lcd_4bit_send_string_pos(&_lcd_1, 1, 1, "Ahmed");
+        ret = lcd_4bit_send_command(&_lcd_1, _LCD_DISPLAY_SHIFT_RIGHT);
+        __delay_ms(250);
+        ret = lcd_4bit_send_command(&_lcd_1, _LCD_DISPLAY_SHIFT_RIGHT);
+        __delay_ms(250);
+        ret = lcd_4bit_send_command(&_lcd_1, _LCD_DISPLAY_SHIFT_LEFT);
+        __delay_ms(250);
+        ret = lcd_4bit_send_command(&_lcd_1, _LCD_DISPLAY_SHIFT_LEFT);
+        __delay_ms(250);
     }
      
     return (EXIT_SUCCESS);
