10584dcd8fdd0d1162070d495d6cb23f28a8732a
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2023-08-29 12:10:59 +0200

    Timer0_timer_mode_test

--- application.c
+++ application.c
@@ -7,41 +7,50 @@
 
 
 #include "application.h"
-void ADC_DefaultInterruptHandler(void);
 
-uint16 adc_result_1;
-volatile uint8 flag;
+led_t led1 = {.port_name = PORTC_INDEX, .pin = GPIO_PIN0, .led_status = GPIO_LOW};
+led_t led2 = {.port_name = PORTC_INDEX, .pin = GPIO_PIN1, .led_status = GPIO_LOW};
 
-adc_config_t adc_1 = {
-    .ADC_InterruptHandler = ADC_DefaultInterruptHandler,
-    .acquisition_time = ADC_12_TAD,
-    .adc_channel = ADC_CHANNEL_AN0,
-    .conversion_clock = ADC_CLOCK_FOSC_DIV_16,
-    .result_format = ADC_RESULT_RIGHT_FORMAT,
-    .voltage_reference = ADC_VOLTAGE_REFERENCE_DISABLED        
-};
+volatile uint8 timer0_250ms = 0, timer0_500ms = 0;
 
-void ADC_DefaultInterruptHandler(void){
-    Std_ReturnType ret = E_NOT_OK;
-    ADC_GetConversionResult(&adc_1, &adc_result_1);
-    flag++;
+void Timer0_DefaultInterruptHandler(void){
+    timer0_250ms = 1;
+    timer0_500ms++;
 }
 
-
+timer0_t timer0_timer_obj = {
+    .TMR0_InterruptHandler = Timer0_DefaultInterruptHandler,
+    .timer0_mode = TIMER0_TIMER_MODE,
+    .timer0_register_size = TIMER0_16BIT_REGISTER_ENABLED,
+    .prescaler_enable = TIMER0_PRESCALER_ENABLED,
+    .prescaler_value = TIMER0_PRESCALER_DIV_BY_4,
+    .timer0_preload_value = 3036
+};
 
 int main() {
     Std_ReturnType ret = E_NOT_OK;
-    application_initialize();
-    ADC_ANALOG_DIGITAL_PORT_CONFIG(ADC_ALL_ANALOG_FUNCTIONALITY);
-   
-    while (1) {
-        ret = ADC_StartConversion_Interrupt(&adc_1, ADC_CHANNEL_AN0);
-    } 
+    
+    ret = Timer0_Init(&timer0_timer_obj);
+    ret = led_initialize(&led1);
+    ret = led_initialize(&led2);
+    
+    while(1){
+        if(timer0_250ms == 1){
+            timer0_250ms = 0;
+            led_toggle(&led1);
+        }
+        else {/* Nothing */}
+        
+        if(timer0_500ms == 2){
+            led_toggle(&led2);
+            timer0_500ms = 0;
+        }
+        else {/* Nothing */}
+    }
     return (EXIT_SUCCESS);
 }
 
 void application_initialize() {
     Std_ReturnType ret = E_NOT_OK;
-    ret = ADC_Init(&adc_1);
     ret = ecu_layer_initialize();
 }
