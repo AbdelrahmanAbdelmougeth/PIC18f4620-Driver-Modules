709548b5ccd75747e45313e28366088cb73928ff
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2023-08-31 12:31:37 +0200

    Timer2_Driver_Test

--- application.c
+++ application.c
@@ -10,51 +10,32 @@
 
 led_t led1 = {.port_name = PORTD_INDEX, .pin = GPIO_PIN0, .led_status = GPIO_LOW};
 
-volatile uint16 timer1_val = 0;
-volatile uint16 counter_val = 0;
+volatile uint16 timer2_val = 0;
 
-void Timer1_DefaultInterruptHandler(void){
-    timer1_val++;
+void Timer2_DefaultInterruptHandler(void){
+    timer2_val++;
     led_toggle(&led1);
 }
 
  timer1_t  counter_obj;
 
-void timer1_timer_init(void){
-    timer1_t  timer_obj;
-    timer_obj.TMR1_InterruptHandler = Timer1_DefaultInterruptHandler;
+void timer2_timer_init(void){
+    timer2_t timer_obj;
+    timer_obj.TMR2_InterruptHandler = Timer2_DefaultInterruptHandler;
     timer_obj.priority = INTERRUPT_LOW_PRIORITY;
-    timer_obj.timer1_mode = TIMER1_TIMER_MODE;
-    timer_obj.timer1_prescaler_value = TIMER1_PRESCALER_DIV_BY_8;
-    timer_obj.timer1_preload_value = 15536;
-    timer_obj.timer1_reg_rw_mode = TIMER1_REG_RW_16BIT_MODE_ENABLED;
-    Timer1_Init(&timer_obj);
+    timer_obj.timer2_postscaler_value = TIMER2_POSTSCALER_DIV_BY_16;
+    timer_obj.timer2_prescaler_value = TIMER2_PRESCALER_DIV_BY_1;
+    timer_obj.timer2_preload_value = 249;
+    Timer2_Init(&timer_obj);
 }
-
-void timer1_counter_init(void){
-   
-   
-}
-
 
 int main() {
     Std_ReturnType ret = E_NOT_OK;
     
     led_initialize(&led1);
-    //timer1_timer_init();
-    timer1_counter_init();
-    
-    counter_obj.TMR1_InterruptHandler = Timer1_DefaultInterruptHandler;
-    counter_obj.priority = INTERRUPT_LOW_PRIORITY;
-    counter_obj.timer1_mode = TIMER1_COUNTER_MODE;
-    counter_obj.timer1_counter_mode = TIMER1_SYNC_COUNTER_MODE;
-    counter_obj.timer1_prescaler_value = TIMER1_PRESCALER_DIV_BY_1;
-    counter_obj.timer1_preload_value = 0;
-    counter_obj.timer1_reg_rw_mode = TIMER1_REG_RW_16BIT_MODE_ENABLED;
-    Timer1_Init(&counter_obj);
-    
+    timer2_timer_init();
+      
     while(1){
-        Timer1_Read_Value(&counter_obj, &counter_val);
     }
     
     return (EXIT_SUCCESS);
--- application.h
+++ application.h
@@ -16,6 +16,7 @@
 #include "MCAL_Layer/hal_adc.h"
 #include "MCAL_Layer/Timer0/hal_timer0.h"
 #include "MCAL_Layer/Timer1/hal_timer1.h"
+#include "MCAL_Layer/Timer2/hal_timer2.h"
 
 /* Section : Macro Declarations */
 
