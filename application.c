7b3c6e281cb8cc971ad1ab92e2e0698ac3f46e80
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2023-09-01 11:39:46 +0200

    Timer3_counter_mode_test

--- application.c
+++ application.c
@@ -8,38 +8,25 @@
 
 #include "application.h"
 
-volatile uint32 timer3_on;
-
-led_t led1 = {.port_name = PORTD_INDEX, .pin = GPIO_PIN0, .led_status = GPIO_LOW};
-
-void Timer3_DefaultInterruptHandler(void){
-    timer3_on++;
-    led_toggle(&led1);
-}
-
-void timer3_timer_init(void){
-    timer3_t timer_obj;
-    timer_obj.TMR3_InterruptHandler = Timer3_DefaultInterruptHandler;
-    timer_obj.priority = INTERRUPT_LOW_PRIORITY;
-    timer_obj.timer3_mode = TIMER3_TIMER_MODE;
-    timer_obj.timer3_prescaler_value = TIMER3_PRESCALER_DIV_BY_8;
-    timer_obj.timer3_preload_value = 15536;
-    timer_obj.timer3_reg_rw_mode = TIMER3_REG_RW_16BIT_MODE_ENABLED;
-    Timer3_Init(&timer_obj);
-}
-
-
+volatile uint16 timer_counter_val;
+timer3_t counter_obj;
 
 int main() {
     Std_ReturnType ret = E_NOT_OK;
     application_initialize();
     
-    timer3_timer_init();
-    ret = led_initialize(&led1);
+    counter_obj.TMR3_InterruptHandler = NULL;
+    counter_obj.priority = INTERRUPT_LOW_PRIORITY;
+    counter_obj.timer3_mode = TIMER3_COUNTER_MODE;
+    counter_obj.timer3_prescaler_value = TIMER3_PRESCALER_DIV_BY_1;
+    counter_obj.timer3_preload_value = 0;
+    counter_obj.timer3_reg_rw_mode = TIMER3_REG_RW_16BIT_MODE_ENABLED;
+    counter_obj.timer3_counter_mode = TIMER3_SYNC_COUNTER_MODE;
+    ret = Timer3_Init(&counter_obj);
     
     while(1){
+        ret = Timer3_Read_Value(&counter_obj, &timer_counter_val);
     }
-    
     return (EXIT_SUCCESS);
 }
 
