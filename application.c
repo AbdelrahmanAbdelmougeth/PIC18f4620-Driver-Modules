28cb930db4380ef2d3896c434817dbfd31db4bfa
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2023-09-12 15:06:07 +0200

    I2C_Master_Mode

--- application.c
+++ application.c
@@ -8,91 +8,44 @@
 
 #include "application.h"
 
-uint8 received_data, sent_data;
-uint32 rx_counter, tx_counter;
+mssp_i2c_t i2c_obj;
+uint8 _slave_ack = 0xff;
 
+#define _slave1 0x60
+#define _slave2 0x61
 
-led_t led1 = {.port_name = PORTD_INDEX, .pin = GPIO_PIN0, .led_status = GPIO_LOW};
-led_t led2 = {.port_name = PORTD_INDEX, .pin = GPIO_PIN1, .led_status = GPIO_LOW};
-
-void EUSART_TxDefaultInterruptHandler(void){
-    tx_counter++;
-}
-
-void EUSART_RxDefaultInterruptHandler(void){
+void MSSP_I2C_Send_1_Byte(uint8 _slave_add, uint8 _data){
     Std_ReturnType ret = E_NOT_OK;
-    ret = EUSART_ASYNC_RecieveByteNonBlocking(&received_data);
-    rx_counter++;
-    switch (received_data){
-        case 'a' : 
-            led_turn_on(&led1);
-            EUSART_ASYNC_SendStringBlocking("Led1 On\r", 8);
-        break;
-        case 'b' : 
-            led_turn_off(&led1);
-            EUSART_ASYNC_SendStringBlocking("Led1 Off\r", 9);
-        break;
-        case 'c' : 
-            led_turn_on(&led2);
-            EUSART_ASYNC_SendStringBlocking("Led2 On\r", 8);
-        break;
-        case 'd' :
-            led_turn_off(&led2);
-            EUSART_ASYNC_SendStringBlocking("Led2 Off\r", 9);
-        break;
-        default: 
-            led_turn_off(&led1); 
-            led_turn_off(&led2);
-            EUSART_ASYNC_SendStringBlocking("Led1 Off\r", 9);
-            EUSART_ASYNC_SendStringBlocking("Led2 Off\r", 9);
-    }
+    ret = MSSP_I2C_Master_Send_Start(&i2c_obj);
+    ret = MSSP_I2C_Master_Write_Blocking(&i2c_obj, _slave_add , &_slave_ack);
+    ret = MSSP_I2C_Master_Write_Blocking(&i2c_obj, _data, &_slave_ack);
+    ret = MSSP_I2C_Master_Send_Stop(&i2c_obj);
 }
-
-void EUSART_FramingErrorHandler(void){
-    if(RCSTAbits.FERR == 1){
-        uint8 _data;
-        EUSART_ASYNC_RecieveByteNonBlocking(&_data);
-    } 
-}
-
-void EUSART_OverrunErrorHandler(void){
-    if(RCSTAbits.OERR == 1){
-        EUSART_ASYNC_RX_Restart();      
-    } 
-}
-
-void eusart_module_init(void){
-    Std_ReturnType ret = E_NOT_OK;
-    eusart_t eusart_obj;
-    eusart_obj.baudrate = 9600;
-    eusart_obj.baudrate_config = BAUDRATE_ASYNC_8BIT_LOW_SPEED;
-    
-    eusart_obj.eusart_tx_cfg.eusart_tx_enable = EUSART_ASYNCHRONOUS_TX_ENABLE;
-    eusart_obj.eusart_tx_cfg.eusart_tx_9bit_enable = EUSART_TX_9BIT_DISABLE;
-    eusart_obj.eusart_tx_cfg.eusart_tx_interrupt_enable = EUSART_ASYNCHRONOUS_INTERRUPT_TX_ENABLE;
-    
-    eusart_obj.eusart_rx_cfg.eusart_rx_enable = EUSART_ASYNCHRONOUS_RX_ENABLE;
-    eusart_obj.eusart_rx_cfg.eusart_rx_9bit_enable = EUSART_RX_9BIT_DISABLE;
-    eusart_obj.eusart_rx_cfg.eusart_rx_interrupt_enable = EUSART_ASYNCHRONOUS_INTERRUPT_RX_ENABLE;
-    
-    eusart_obj.EUSART_TxDefaultInterruptHandler = EUSART_TxDefaultInterruptHandler;
-    eusart_obj.EUSART_RxDefaultInterruptHandler = EUSART_RxDefaultInterruptHandler;
-    eusart_obj.EUSART_FramingErrorHandler = EUSART_FramingErrorHandler;
-    eusart_obj.EUSART_OverrunErrorHandler = EUSART_OverrunErrorHandler;
-    
-    ret = EUSART_ASYNC_Init(&eusart_obj);
-}
-
 
 int main() {
     Std_ReturnType ret = E_NOT_OK;
     application_initialize();
     
-    eusart_module_init();
-    led_initialize(&led1);
-    led_initialize(&led2);
+    i2c_obj.i2c_clock = 100000;
+    i2c_obj.i2c_cfg.i2c_mode = MSSP_I2C_MASTER_MODE;
+    i2c_obj.i2c_cfg.i2c_mode_cfg = I2C_MASTER_MODE_DEFINED_CLOCK;
+    i2c_obj.i2c_cfg.i2c_SMBus_control = I2C_SMBus_DISABLED;
+    i2c_obj.i2c_cfg.i2c_slew_rate = I2C_SLEW_RATE_DISABLED;
+    i2c_obj.i2c_cfg.i2c_master_receive_mode = I2C_MASTER_MODE_RECEIVE_DISABLED;
+    i2c_obj.I2C_DefaultInterruptHandler = NULL;
+    i2c_obj.I2C_Report_Receive_Overflow = NULL;
+    i2c_obj.I2C_Report_Write_Collision = NULL;
+    ret = MSSP_I2C_Init(&i2c_obj);
     
     while(1){  
+        MSSP_I2C_Send_1_Byte(_slave1, 'a');
+        __delay_ms(1000);
+        MSSP_I2C_Send_1_Byte(_slave2, 'b');
+        __delay_ms(1000);
+        MSSP_I2C_Send_1_Byte(_slave1, 'c');
+        __delay_ms(1000);
+        MSSP_I2C_Send_1_Byte(_slave2, 'd');
+        __delay_ms(1000);
     }
     
     return (EXIT_SUCCESS);
--- application.h
+++ application.h
@@ -12,14 +12,15 @@
 /* Section : Includes */
 #include "ECU_Layer/ecu_layer_init.h"
 #include "MCAL_Layer/Interrupt/mcal_external_interrupt.h"
-#include "MCAL_Layer/EEPROM/hal_eeprom.h"
-#include "MCAL_Layer/ADC/hal_adc.h"
-#include "MCAL_Layer/Timer0/hal_timer0.h"
-#include "MCAL_Layer/Timer1/hal_timer1.h"
-#include "MCAL_Layer/Timer2/hal_timer2.h"
-#include "MCAL_Layer/Timer3/hal_timer3.h"
-#include "MCAL_Layer/CCP1/hal_ccpx.h"
-#include "MCAL_Layer/usart/hal_usart.h"
+//#include "MCAL_Layer/EEPROM/hal_eeprom.h"
+//#include "MCAL_Layer/ADC/hal_adc.h"
+//#include "MCAL_Layer/Timer0/hal_timer0.h"
+//#include "MCAL_Layer/Timer1/hal_timer1.h"
+//#include "MCAL_Layer/Timer2/hal_timer2.h"
+//#include "MCAL_Layer/Timer3/hal_timer3.h"
+//#include "MCAL_Layer/CCP1/hal_ccpx.h"
+//#include "MCAL_Layer/usart/hal_usart.h"
+#include "MCAL_Layer/I2C/hal_i2c.h"
 
 /* Section : Macro Declarations */
 
