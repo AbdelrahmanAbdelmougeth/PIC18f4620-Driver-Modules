e9728753b17b86a78dc0573c056587c216677a8d
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2023-09-12 17:03:45 +0200

    I2C_SLAVE2_INTERRUPT_ADDED_TEST

--- application.c
+++ application.c
@@ -13,8 +13,8 @@ mssp_i2c_t i2c_obj;
 #define _slave1 0x60
 #define _slave2 0x62
 
-volatile uint8 i2c_slave1_rec_counter;
-static volatile uint8 i2c_slave1_rec_data;
+volatile uint8 i2c_slave2_rec_counter;
+static volatile uint8 i2c_slave2_rec_data;
 
 void MSSP_I2C_DefaultInterruptHandler(){
     /* Stretch SCK To force the master to stay idle and don't send any data */
@@ -24,7 +24,7 @@ void MSSP_I2C_DefaultInterruptHandler(){
     if(SSPSTATbits.R_nW == 0 && SSPSTATbits.D_nA == 0){
         uint8 dummy_buffer = SSPBUF;  /* read the last byte to clear the buffer */
         while(!I2C_GET_SSPBUF_STATUS()); /* wait for the data to be completely received */
-        i2c_slave1_rec_data = SSPBUF; /* read the data */
+        i2c_slave2_rec_data = SSPBUF; /* read the data */
     }else if(SSPSTATbits.R_nW = 1){
         
     }else{ /* nothing */ }
@@ -32,7 +32,7 @@ void MSSP_I2C_DefaultInterruptHandler(){
     /* SCK Stretch disable */
     I2C_CLOCK_STRETCH_DISABLE();
     
-    i2c_slave1_rec_counter++;
+    i2c_slave2_rec_counter++;
 }
 
 led_t led1 = {.port_name = PORTD_INDEX, .pin = GPIO_PIN0, .led_status = GPIO_LOW};
@@ -44,7 +44,7 @@ int main() {
     i2c_obj.i2c_cfg.i2c_mode = MSSP_I2C_SLAVE_MODE;
     i2c_obj.i2c_cfg.i2c_mode_cfg = I2C_SLAVE_MODE_7_BIT_ADDRESS;
     i2c_obj.i2c_cfg.i2c_SMBus_control = I2C_SMBus_DISABLED;
-    i2c_obj.i2c_cfg.i2c_slave_add = _slave1;
+    i2c_obj.i2c_cfg.i2c_slave_add = _slave2;
     i2c_obj.i2c_cfg.i2c_slew_rate = I2C_SLEW_RATE_DISABLED;
     i2c_obj.i2c_cfg.i2c_general_call = I2C_GENERAL_CALL_DISABLED;
     i2c_obj.I2C_DefaultInterruptHandler = MSSP_I2C_DefaultInterruptHandler;
@@ -54,9 +54,9 @@ int main() {
     led_initialize(&led1);
     
     while(1){  
-        if(i2c_slave1_rec_data == 'a'){
+        if(i2c_slave2_rec_data == 'b'){
             led_turn_on(&led1);
-        }else if(i2c_slave1_rec_data == 'c'){
+        }else if(i2c_slave2_rec_data == 'd'){
             led_turn_off(&led1);
         } 
     }
