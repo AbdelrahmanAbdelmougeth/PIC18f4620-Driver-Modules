509aa3640ab70b68dc31de41a6c2c53698fda918
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2023-09-15 14:03:47 +0200

    SPI_Slave2_Project

--- application.c
+++ application.c
@@ -8,12 +8,12 @@
 
 #include "application.h"
 
-uint8 receved_data;
+uint8 received_data;
 
 void MSSP_SPI_DefaultInterruptHandler(){
 }
 
-spi_t spi_slave1_obj = {
+spi_t spi_slave2_obj = {
     .spi_mode = SPI_SLAVE_MODE_SS_ENABLED,
     .spi_config.ClockPolarity = CLOCK_POLARITY_IDLE_IS_LOW,
     .spi_config.ClockSelect = TRANSMIT_FROM_IDLE_TO_ACTIVE,
@@ -30,14 +30,14 @@ int main() {
     Std_ReturnType ret = E_NOT_OK;
     application_initialize();
     
-    ret = SPI_Init(&spi_slave1_obj);
+    ret = SPI_Init(&spi_slave2_obj);
     ret = led_initialize(&led1);
     
     while(1){
-        ret = SPI_Read_Byte_Blocking(&spi_slave1_obj, &receved_data);
-        if(receved_data == 'a'){
+        ret = SPI_Read_Byte_Blocking(&spi_slave2_obj, &received_data);
+        if(received_data == 'b'){
             led_turn_on(&led1);
-        }else if(receved_data == 'c'){
+        }else if(received_data == 'd'){
             led_turn_off(&led1);
         }
     }
