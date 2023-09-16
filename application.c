64f8494bdd9a7b6647b633dc3ff6c91c339ed109
Abdelrahman_Abdelmougeth <abdelrahmana966@gmail.com>
2023-09-16 22:05:03 +0300

    SPI_Non_Blocking_Interrupt_Feature_Slave_1

--- application.c
+++ application.c
@@ -8,62 +8,38 @@
 
 #include "application.h"
 
-volatile uint8 interrupt_flag_counter = 0;
-volatile uint8 send_not_completed_flag = 0;
+volatile uint8 receveid_data;
+void MSSP_SPI_DefaultInterruptHandler();
 
-led_t SPI_SS1 = {.port_name = PORTD_INDEX, .pin = GPIO_PIN0, .led_status = GPIO_HIGH};
-led_t SPI_SS2 = {.port_name = PORTD_INDEX, .pin = GPIO_PIN1, .led_status = GPIO_HIGH};
-
-spi_t spi_master_obj;
-
+spi_t spi_slave1_obj = {
+    .spi_mode = SPI_SLAVE_MODE_SS_ENABLED,
+    .spi_config.ClockPolarity = CLOCK_POLARITY_IDLE_IS_LOW,
+    .spi_config.ClockSelect = TRANSMIT_FROM_IDLE_TO_ACTIVE,
+    .spi_config.SampleSelect = SAMPLE_INPUT_DATA_AT_MID,
+    #if MSSP_SPI_INTERRUPT_FEATURE_ENABLE == INTERRUPT_FEATURE_ENABLE
+        .MSSP_SPI_InterruptHandler = MSSP_SPI_DefaultInterruptHandler,
+        .priority = INTERRUPT_LOW_PRIORITY,
+    #endif
+};
 
 void MSSP_SPI_DefaultInterruptHandler(){
-    send_not_completed_flag = 0;
-    interrupt_flag_counter++;
-    if(interrupt_flag_counter == 4){
-        interrupt_flag_counter = 0;
-    }
-    led_turn_on(&SPI_SS1);
-    led_turn_on(&SPI_SS2);
+    SPI_Send_Byte_NonBlocking(&spi_slave1_obj, &receveid_data);
 }
+
+led_t led1 = {.port_name = PORTD_INDEX, .pin = GPIO_PIN0, .led_status = GPIO_LOW};
 
 int main() {
     Std_ReturnType ret = E_NOT_OK;
     application_initialize();
     
-    spi_master_obj.spi_mode = SPI_MASTER_MODE_FOSC_DIV_4;
-    spi_master_obj.spi_config.ClockPolarity = CLOCK_POLARITY_IDLE_IS_LOW;
-    spi_master_obj.spi_config.ClockSelect = TRANSMIT_FROM_IDLE_TO_ACTIVE;
-    spi_master_obj.spi_config.SampleSelect = SAMPLE_INPUT_DATA_AT_MID;
-    #if MSSP_SPI_INTERRUPT_FEATURE_ENABLE == INTERRUPT_FEATURE_ENABLE
-        spi_master_obj.MSSP_SPI_InterruptHandler = MSSP_SPI_DefaultInterruptHandler,
-        spi_master_obj.priority = INTERRUPT_LOW_PRIORITY,
-    #endif
+    ret = SPI_Init(&spi_slave1_obj);
+    ret = led_initialize(&led1);
     
-    ret = SPI_Init(&spi_master_obj);
-    ret = led_initialize(&SPI_SS1);
-    ret = led_initialize(&SPI_SS2);
-        
-    while(1){       
-        if(!send_not_completed_flag && interrupt_flag_counter == 0){         
-            ret = led_turn_off(&SPI_SS1);
-            ret = SPI_Send_Byte_NonBlocking(&spi_master_obj, 'a');
-            send_not_completed_flag = 1;
-        }
-        else if(!send_not_completed_flag && interrupt_flag_counter == 1){
-            ret = led_turn_off(&SPI_SS2);
-            ret = SPI_Send_Byte_NonBlocking(&spi_master_obj, 'b');
-            send_not_completed_flag = 1;
-        }
-        else if(!send_not_completed_flag && interrupt_flag_counter == 2){
-            ret = led_turn_off(&SPI_SS1);
-            ret = SPI_Send_Byte_NonBlocking(&spi_master_obj, 'c');
-            send_not_completed_flag = 1;
-        }
-        else if(!send_not_completed_flag && interrupt_flag_counter == 3){
-            ret = led_turn_off(&SPI_SS2);
-            ret = SPI_Send_Byte_NonBlocking(&spi_master_obj, 'd');
-            send_not_completed_flag = 1;
+    while(1){
+        if(receveid_data == 'a'){
+            led_turn_on(&led1);
+        }else if(receveid_data == 'c'){
+            led_turn_off(&led1);
         }
     }
     
